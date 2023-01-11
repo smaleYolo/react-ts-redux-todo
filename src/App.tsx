c2dae4813c329c9c89d6c1eef46ae3ab11d61712
smaleYolo <ilia.artamonov@yandex.ru>
2023-01-11 23:46:32 +0300

    gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React, {useState} from 'react';
-import TodoList from "./components/TodoList";
-import NewTodoForm from "./components/NewTodoForm";
 import Navigation from "./components/Navigation";
-import TodoClasses from "./components/TodoClasses";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import TodosPage from "./pages/TodosPage";
 import Login from "./pages/Login";
-import login from "./pages/Login";
 
 export type UserLogin = {
     login: string,
@@ -30,6 +26,7 @@ function App() {
             {isAuth ? (
                 <Routes>
                     <Route path="/" element={<TodosPage/>}/>
+                    <Route path="*" element={<TodosPage/>}/>
                 </Routes>
             ) : (
                 <Routes>
